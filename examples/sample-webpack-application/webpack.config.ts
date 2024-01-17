c48159c53afa41f4ff15547ac665511cc296691c
Dmitriy Shekhovtsov <valorkin@gmail.com>
2024-01-17 11:52:03 +0100

    chore: cli is good enough

---
+++ examples/sample-webpack-application/webpack.config.ts
@@ -0,0 +1,15 @@
+import { composePlugins, withNx } from '@nx/webpack';
+
+import { withReact } from '@nx/react';
+
+import { withZephyr } from 'zephyr-webpack-plugin';
+
+// Nx plugins for webpack.
+module.exports = composePlugins(
+  withNx(),
+  withReact({}),
+  withZephyr(),
+  (config) => {
+    return config;
+  },
+);

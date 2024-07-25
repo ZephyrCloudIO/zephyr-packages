64bdb8ed42011b9f9a7a274102fe1c449185f192
Rodrigo Yokota <53323214+ryok90@users.noreply.github.com>
2024-07-25 07:38:19 -0300

    fix: make wrnagler as optional dependency on ze agent (#107)

--- libs/zephyr-agent/src/lib/upload/strategies/cloudflare.strategy.ts
+++ libs/zephyr-agent/src/lib/upload/strategies/cloudflare.strategy.ts
@@ -1,4 +1,3 @@
-import { unstable_pages } from 'wrangler';
 import * as process from 'process';
 import { access, constants, mkdir, writeFile } from 'fs/promises';
 import { dirname, sep } from 'path';
@@ -26,16 +25,14 @@ import {
 } from '../../actions';
 import { UploadOptions } from '../upload';
 
-export async function cloudflareStrategy(
-  {
-    pluginOptions,
-    getDashData,
-    appConfig,
-    zeStart,
-    assets: {assetsMap, missingAssets, count, outputPath},
-    uploadConfig,
-  }: UploadOptions
-): Promise<boolean> {
+export async function cloudflareStrategy({
+  pluginOptions,
+  getDashData,
+  appConfig,
+  zeStart,
+  assets: { assetsMap, missingAssets, count, outputPath },
+  uploadConfig,
+}: UploadOptions): Promise<boolean> {
   const snapshot = createSnapshot({
     options: pluginOptions,
     assets: assetsMap,
@@ -45,13 +42,17 @@ export async function cloudflareStrategy(
 
   await Promise.all([
     zeUploadSnapshot(pluginOptions, snapshot),
-    uploadAssets({assetsMap, missingAssets, pluginOptions, count}),
+    uploadAssets({ assetsMap, missingAssets, pluginOptions, count }),
   ]);
 
-  const envs = await uploadBuildStatsAndEnableEnvs({appConfig, pluginOptions, getDashData});
+  const envs = await uploadBuildStatsAndEnableEnvs({
+    appConfig,
+    pluginOptions,
+    getDashData,
+  });
 
   if (!envs) {
-    ze_error("ZE10016", 'Did not receive envs from build stats upload.');
+    ze_error('ZE10016', 'Did not receive envs from build stats upload.');
 
     return false;
   }
@@ -80,7 +81,12 @@ interface UploadAssetsOptions {
   count: number;
 }
 
-async function uploadAssets({assetsMap, missingAssets, pluginOptions, count}: UploadAssetsOptions) {
+async function uploadAssets({
+  assetsMap,
+  missingAssets,
+  pluginOptions,
+  count,
+}: UploadAssetsOptions) {
   const upload_success = await zeUploadAssets(pluginOptions, {
     missingAssets,
     assetsMap,
@@ -99,8 +105,12 @@ interface UploadBuildStatsAndEnableEnvsOptions {
   getDashData: (options: GetDashDataOptions) => unknown;
 }
 
-async function uploadBuildStatsAndEnableEnvs({appConfig, pluginOptions, getDashData}: UploadBuildStatsAndEnableEnvsOptions) {
-  const dashData = getDashData({appConfig, pluginOptions});
+async function uploadBuildStatsAndEnableEnvs({
+  appConfig,
+  pluginOptions,
+  getDashData,
+}: UploadBuildStatsAndEnableEnvsOptions) {
+  const dashData = getDashData({ appConfig, pluginOptions });
 
   return zeUploadBuildStats(dashData);
 }
@@ -108,27 +118,48 @@ async function uploadBuildStatsAndEnableEnvs({appConfig, pluginOptions, getDashD
 interface UploadToPagesOptions {
   uploadConfig: UploadProviderConfig;
   outputPath: string;
-  assetsMap: ZeBuildAssetsMap,
+  assetsMap: ZeBuildAssetsMap;
   pluginOptions: ZephyrPluginOptions;
   envs: ZeUploadBuildStats;
 }
 
-async function uploadToPages({uploadConfig, pluginOptions, outputPath, assetsMap, envs}: UploadToPagesOptions) {
+async function uploadToPages({
+  uploadConfig,
+  pluginOptions,
+  outputPath,
+  assetsMap,
+  envs,
+}: UploadToPagesOptions) {
   if (!uploadConfig.providerConfig.projectName) {
     return;
   }
 
   return saveAssetsToFilesIfNotExist(outputPath, assetsMap)
-    .then(outputPath => upload(outputPath, uploadConfig.providerConfig))
-    .then(pages_url => enablePages(pluginOptions, envs, pages_url))
+    .then((outputPath) => upload(outputPath, uploadConfig.providerConfig))
+    .then((pages_url) => enablePages(pluginOptions, envs, pages_url))
     .then(() => ze_log('Build deployed to cloudflare pages'))
-    .catch(error => ze_error(`Error upload to cloudflare pages: ${error.message}`));
+    .catch((error) =>
+      ze_error(`Error upload to cloudflare pages: ${error.message}`)
+    );
 }
 
-async function upload(outputPath: string, {api_token, accountId, projectName}: CloudflareOptions): Promise<string> {
+async function upload(
+  outputPath: string,
+  { api_token, accountId, projectName }: CloudflareOptions
+): Promise<string> {
   process.env['CLOUDFLARE_API_TOKEN'] = api_token;
 
-  const result = await unstable_pages.deploy({
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const wrangler = require('wrangler');
+
+  if (!wrangler) {
+    ze_error(
+      'Wrangler dependency is needed for Cloudflare deployment. Please install dependencies without --no-optional flag.'
+    );
+    throw new Error('Wrangler dependency not installed.');
+  }
+
+  const result = await wrangler.unstable_pages.deploy({
     directory: outputPath,
     accountId,
     projectName: projectName as string,
@@ -140,21 +171,28 @@ async function upload(outputPath: string, {api_token, accountId, projectName}: C
   return result.url;
 }
 
-async function enablePages(pluginOptions: ZephyrPluginOptions, buildStats: ZeUploadBuildStats, pages_url: string) {
+async function enablePages(
+  pluginOptions: ZephyrPluginOptions,
+  buildStats: ZeUploadBuildStats,
+  pages_url: string
+) {
   return zeEnableSnapshotOnPages({
     pluginOptions,
     envs_jwt: buildStats,
     pages_url,
-  })
+  });
 }
 
-async function saveAssetsToFilesIfNotExist(dir: string, assetsMap: ZeBuildAssetsMap): Promise<string> {
+async function saveAssetsToFilesIfNotExist(
+  dir: string,
+  assetsMap: ZeBuildAssetsMap
+): Promise<string> {
   try {
     await access(dir, constants.R_OK | constants.W_OK);
   } catch (error) {
     ze_log(`Dist folder doesn't exist, creating`);
     try {
-      await mkdir(dir, {recursive: true});
+      await mkdir(dir, { recursive: true });
     } catch (error) {
       ze_error(`Error creating dist folder: ${(error as Error).message}`);
       throw new Error('Unable to create dist folder.');
@@ -162,13 +200,16 @@ async function saveAssetsToFilesIfNotExist(dir: string, assetsMap: ZeBuildAssets
   }
 
   const promises: Promise<void>[] = [];
-  for (const [, {path, buffer}] of Object.entries(assetsMap)) {
+  for (const [, { path, buffer }] of Object.entries(assetsMap)) {
     const fullPath = `${dir}${sep}${path}`;
     if (path.includes('/')) {
-      promises.push(mkdir(dirname(fullPath), {recursive: true})
-        .then(() => writeFile(`${dir}/${path}`, buffer, {flag: 'w+'})));
+      promises.push(
+        mkdir(dirname(fullPath), { recursive: true }).then(() =>
+          writeFile(`${dir}/${path}`, buffer, { flag: 'w+' })
+        )
+      );
     } else {
-      promises.push(writeFile(`${dir}/${path}`, buffer, {flag: 'w+'}));
+      promises.push(writeFile(`${dir}/${path}`, buffer, { flag: 'w+' }));
     }
   }
 

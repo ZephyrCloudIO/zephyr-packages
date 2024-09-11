8f9c04361965397ddc31bd163c339504b1323a46
Valerii Tropin <valeriitropin@users.noreply.github.com>
2024-09-11 13:46:22 +0300

    fix: convert application uid to lower case (#155)

--- libs/zephyr-edge-contract/src/lib/utils/create-application-u-i-d.ts
+++ libs/zephyr-edge-contract/src/lib/utils/create-application-u-i-d.ts
@@ -3,13 +3,9 @@ function replacer(str: string): string {
   // return str.replace(/\W/gi, '_');
 }
 
-export function createApplicationUID(options: {
-  org: string;
-  project: string;
-  name: string;
-}): string {
+export function createApplicationUID(options: { org: string; project: string; name: string }): string {
   const git_org = replacer(options.org);
   const git_repo = replacer(options.project);
   const app_name = replacer(options.name);
-  return [app_name, git_repo, git_org].join('.');
+  return [app_name, git_repo, git_org].join('.').toLowerCase();
 }

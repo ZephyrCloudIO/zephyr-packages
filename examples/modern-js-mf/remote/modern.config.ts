76bc79c4f441854ae73a3a88b03f5d839de80f02
Néstor <nestor@nstlopez.com>
2025-01-30 21:25:50 +0100

    chore:use zephyr in examples

--- examples/modern-js-mf/remote/modern.config.ts
+++ examples/modern-js-mf/remote/modern.config.ts
@@ -1,10 +1,22 @@
 import { appTools, defineConfig } from '@modern-js/app-tools';
 import { moduleFederationPlugin } from '@module-federation/modern-js';
+import { withZephyr } from 'zephyr-modernjs-plugin';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
   dev: {
     port: 3051,
+  },
+  output: {
+    distPath: {
+      html: './',
+    },
+  },
+  html: {
+    outputStructure: 'flat',
+  },
+  source: {
+    mainEntryName: 'index',
   },
   runtime: {
     router: true,
@@ -14,5 +26,6 @@ export default defineConfig({
       bundler: 'rspack', // Set to 'webpack' to enable webpack
     }),
     moduleFederationPlugin(),
+    withZephyr(),
   ],
 });

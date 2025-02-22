0c8ea9c7d47470e117825342df37b52b8428175b
Néstor <nestor@nstlopez.com>
2025-02-22 16:46:01 +0100

    feat: add example of mf with rolldown

---
+++ examples/rolldown-mf/remote/rolldown.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'rolldown';
+import { moduleFederationPlugin } from 'rolldown/experimental';
+import { withZephyr } from 'zephyr-rolldown-plugin';
+
+// TODO: can't resolve `./Button.jsx` at ubuntu.
+export default defineConfig({
+  input: './button.jsx',
+  plugins: [
+    moduleFederationPlugin({
+      name: 'mf-remote',
+      filename: 'remote-entry.js',
+      exposes: {
+        './button': './button.jsx',
+      },
+      shared: {
+        react: {
+          singleton: true,
+        },
+      },
+      manifest: true,
+      getPublicPath: 'http://localhost:8085/',
+    }),
+    withZephyr(),
+  ],
+});

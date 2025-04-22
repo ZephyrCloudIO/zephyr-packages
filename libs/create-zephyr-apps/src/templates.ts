b4ef1f9112dbaba58ccf668bf01a4059b9893f3c
Arthur Fiorette <me@arthur.place>
2025-04-22 09:31:06 -0300

    templates

--- libs/create-zephyr-apps/src/templates.ts
+++ libs/create-zephyr-apps/src/templates.ts
@@ -11,83 +11,61 @@ export const ProjectTypes = [
   },
 ];
 
-export const Templates = [
+// TODO: Programmatically load templates from the examples repo after cloning it
+export const Templates: { name: string; label: string; hint: string }[] = [
   {
     name: 'angular-vite',
     label: 'Angular app with Vite',
     hint: 'You will be building an Angular app powered by Vite.',
-    framework: 'angular',
   },
   {
     name: 'modernjs-app',
     label: 'ModernJS',
     hint: 'A simple ModernJS app.',
-    framework: 'react',
-  },
-  {
-    name: 'ng-nx',
-    label: 'Angular app with Nx',
-    hint: 'An Angular application with Nx as Monorepo manager.',
-    framework: 'angular',
-  },
-  {
-    name: 'nx-rspack-mf',
-    label: 'React + Nx + Rspack',
-    hint: 'A React application with Module Federation, using Nx as Monorepo manager and Rspack as the bundler.',
-    framework: 'react',
-  },
-  {
-    name: 'nx-webpack-mf',
-    label: 'React + Nx + Webpack',
-    hint: 'React applications with Module Federation, using Nx as Monorepo manager and Webpack as the bundler.',
-    framework: 'react',
   },
   {
     name: 'qwik-1.5',
     label: 'Qwik + Vite',
     hint: 'A Qwik v1.5 app using Vite as the bundler.',
-    framework: 'qwik',
   },
   {
     name: 'react-rspack-tractor-2.0',
     label: 'React + Rspack + Tractor 2.0',
     hint: 'A React application using Rspack as the bundler and Tractor 2.0 as the module federation manager.',
-    framework: 'react',
   },
   {
     name: 'react-vite-mf',
     label: 'React + Vite + Webpack + Rspack',
     hint: 'You will be building federated React apps powered by Vite, Webpack and Rspack.',
-    framework: 'react',
   },
   {
     name: 'react-vite-ts',
     label: 'A simple React application build by Vite',
     hint: 'You will be building React app powered by Vite.',
-    framework: 'react',
-  },
-  {
-    name: 'react-webpack-mf',
-    label: 'React + Webpack',
-    hint: 'A React application with Module Federation, using Webpack as the bundler.',
-    framework: 'react',
   },
   {
     name: 'rolldown-react',
     label: 'React + Rolldown',
     hint: 'A React example using Rolldown.',
-    framework: 'react',
   },
   {
     name: 'rspack-project',
     label: 'React + Rspack',
     hint: 'A simple application build by Rspack.',
-    framework: 'react',
+  },
+  {
+    name: 'solid',
+    label: 'Solid + Vite',
+    hint: 'A Solid app using Vite as the bundler.',
+  },
+  {
+    name: 'svelte',
+    label: 'Svelte + Vite',
+    hint: 'A Svelte app using Vite as the bundler.',
   },
   {
     name: 'turbo-rspack-mf',
     label: 'Turbo + Rspack + Module Federation',
     hint: 'A monorepo using Turborepo, React, and Rspack as the bundler.',
-    framework: 'react',
   },
-];
+].sort((a, b) => a.name.localeCompare(b.name));

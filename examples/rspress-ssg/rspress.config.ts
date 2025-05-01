177d2e5c0301c7108e8c114a38e5b1ae1ae7629c
Rodrigo Yokota <ryok90@gmail.com>
2025-05-01 09:31:18 -0300

    fix: on after build hook usage

--- examples/rspress-ssg/rspress.config.ts
+++ examples/rspress-ssg/rspress.config.ts
@@ -1,14 +1,32 @@
+import type { RsbuildPlugin } from '@rsbuild/core';
+import type {  StatsCompilation } from '@rspack/core';
 import * as path from 'node:path';
-const { defineConfig } = require('rspress/config');
-const { withZephyr } = require('zephyr-rspack-plugin');
+import { defineConfig } from 'rspress/config';
+import { withZephyr } from 'zephyr-rspack-plugin';
 
-const zephyrRsbuildPlugin = () => ({
+const zephyrRsbuildPlugin = (): RsbuildPlugin => ({
   name: 'zephyr-rsbuild-plugin',
-  setup(api: { modifyRspackConfig: (config: any) => Promise<void> }) {
-    api.modifyRspackConfig(async (config: any) => {
-      // this is important to avoid multiple zephyr build triggers
-      config.name === 'web' && (await withZephyr()(config));
-    });
+  setup(api) {
+    // api.modifyRspackConfig(async (config: any) => {
+    //   // this is important to avoid multiple zephyr build triggers
+    //   config.name === 'web' && (await withZephyr()(config));
+    // });
+    api.onAfterBuild(async ({ stats }) => {
+      if (!stats) return;
+      const compilation: StatsCompilation | undefined = stats.toJson({ all: false, assets: true }).children?.[0] || stats.toJson({ all: false, assets: true });
+
+      if (!compilation?.assets) {
+        console.warn('No assets found in compilation stats.');
+        return;
+      }
+
+      const outputFiles = compilation.assets.map((asset) => asset.name);
+
+      console.log('Emitted output files:');
+      for (const file of outputFiles) {
+        console.log(file);
+      }
+    })
   },
 });
 

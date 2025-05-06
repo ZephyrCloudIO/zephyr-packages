b6dfb00a5f2e2a2d850f7da0c7f9152d6e119535
Néstor <nestor@nstlopez.com>
2025-05-06 20:12:13 +0200

    feat: add mf example

---
+++ examples/rolldown-mf/host/rolldown.config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig } from 'rolldown';
+import { moduleFederationPlugin } from 'rolldown/experimental';
+
+export default defineConfig({
+  input: './index.jsx',
+  plugins: [
+    moduleFederationPlugin({
+      name: 'rolldown-host',
+      remotes: {
+        rolldown: 'http://localhost:8085/mf-manifest.json',
+      },
+      shared: {
+        react: {
+          singleton: true,
+        },
+      },
+    }),
+    {
+      name: 'emit-html',
+      generateBundle() {
+        const html = `
+          <html>
+            <body>
+              <div id="app"></div>
+              <script type="module" src="./index.js"></script>
+            </body>
+          </html>
+        `;
+        this.emitFile({
+          type: 'asset',
+          fileName: 'index.html',
+          source: html,
+        });
+      },
+    },
+  ],
+});
---
+++ examples/rolldown-mf/remote/rolldown.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'rolldown';
+import { moduleFederationPlugin } from 'rolldown/experimental';
+
+// TODO: can't resolve `./Button.jsx` at ubuntu.
+export default defineConfig({
+  input: './button.jsx',
+  plugins: [
+    moduleFederationPlugin({
+      name: 'rolldown-remote',
+      filename: 'remote-entry.js',
+      exposes: {
+        './button': './button.jsx',
+      },
+      shared: {
+        react: {
+          singleton: true,
+        },
+      },
+      manifest: true,
+      getPublicPath: 'http://localhost:8085/',
+    }),
+  ],
+});

98c6a957a8f1d9685807ccbab113cf68b952608f
Arthur Fiorette <me@arthur.place>
2025-05-07 11:53:26 -0300

    Wrap create calls()

--- libs/zephyr-rspack-plugin/src/rspack-plugin/with-zephyr.ts
+++ libs/zephyr-rspack-plugin/src/rspack-plugin/with-zephyr.ts
@@ -1,12 +1,12 @@
-import { Configuration as RspackConfiguration } from '@rspack/core';
-import { ZephyrEngine } from 'zephyr-agent';
-import { ZeRspackPlugin } from './ze-rspack-plugin';
-import { ZephyrRspackPluginOptions } from '../types';
+import type { Configuration as RspackConfiguration } from '@rspack/core';
+import { ZephyrEngine, ZephyrError, logFn } from 'zephyr-agent';
 import {
   extractFederatedDependencyPairs,
   makeCopyOfModuleFederationOptions,
   mutWebpackFederatedRemotesConfig,
 } from 'zephyr-xpack-internal';
+import type { ZephyrRspackPluginOptions } from '../types';
+import { ZeRspackPlugin } from './ze-rspack-plugin';
 
 export type Configuration = RspackConfiguration;
 
@@ -20,28 +20,32 @@ async function _zephyr_configuration(
   config: Configuration,
   _zephyrOptions?: ZephyrRspackPluginOptions
 ): Promise<Configuration> {
-  // create instance of ZephyrEngine to track the application
-  const zephyr_engine = await ZephyrEngine.create({
-    builder: 'rspack',
-    context: config.context,
-  });
+  try {
+    // create instance of ZephyrEngine to track the application
+    const zephyr_engine = await ZephyrEngine.create({
+      builder: 'rspack',
+      context: config.context,
+    });
 
-  // Resolve dependencies and update the config
-  const dependencyPairs = extractFederatedDependencyPairs(config);
+    // Resolve dependencies and update the config
+    const dependencyPairs = extractFederatedDependencyPairs(config);
 
-  const resolved_dependency_pairs =
-    await zephyr_engine.resolve_remote_dependencies(dependencyPairs);
+    const resolved_dependency_pairs =
+      await zephyr_engine.resolve_remote_dependencies(dependencyPairs);
 
-  mutWebpackFederatedRemotesConfig(zephyr_engine, config, resolved_dependency_pairs);
+    mutWebpackFederatedRemotesConfig(zephyr_engine, config, resolved_dependency_pairs);
 
-  // inject the ZephyrRspackPlugin
-  config.plugins?.push(
-    new ZeRspackPlugin({
-      zephyr_engine,
-      mfConfig: makeCopyOfModuleFederationOptions(config),
-      wait_for_index_html: _zephyrOptions?.wait_for_index_html,
-    })
-  );
+    // inject the ZephyrRspackPlugin
+    config.plugins?.push(
+      new ZeRspackPlugin({
+        zephyr_engine,
+        mfConfig: makeCopyOfModuleFederationOptions(config),
+        wait_for_index_html: _zephyrOptions?.wait_for_index_html,
+      })
+    );
+  } catch (error) {
+    logFn('error', ZephyrError.format(error));
+  }
 
   return config;
 }

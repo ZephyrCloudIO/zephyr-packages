c2374a321c3dcb010ea9ab46def95b2235cce8c3
Arthur Fiorette <me@arthur.place>
2025-05-09 18:36:51 -0300

    feat: code

--- libs/zephyr-agent/src/lib/node-persist/fs-cache.ts
+++ libs/zephyr-agent/src/lib/node-persist/fs-cache.ts
@@ -1,15 +1,10 @@
-import { getItem, init, removeItem, setItem } from 'node-persist';
-import { join } from 'node:path';
-import { homedir } from 'node:os';
-import { StorageKeys, ZE_PATH } from './storage-keys';
-
-const storage = init({
-  dir: join(homedir(), ZE_PATH),
-});
+import { getItem, removeItem, setItem } from 'node-persist';
+import { storage } from './storage';
+import { StorageKeys } from './storage-keys';
 
 export async function saveCache(key: string, value: string): Promise<void> {
   await storage;
-  void (await setItem(`${StorageKeys.ze_fs_cache}:${key}`, value));
+  await setItem(`${StorageKeys.ze_fs_cache}:${key}`, value);
 }
 
 export async function getCache(key: string): Promise<string | undefined> {

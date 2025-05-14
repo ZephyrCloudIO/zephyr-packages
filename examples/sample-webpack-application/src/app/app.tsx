c91f582ab4008cae8a844b485de2c6d20110b59a
Fiorin <luciano@fior.in>
2025-05-14 09:55:25 -0300

    fix: changing the way to use env vars

--- examples/sample-webpack-application/src/app/app.tsx
+++ examples/sample-webpack-application/src/app/app.tsx
@@ -1,12 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
 import NxWelcome from './nx-welcome';
 
 export function App() {
-  const title = window.__ENV__?.ZE_ENV_TITLE || 'Fallback Title';
-  const description =
-    window.__ENV__?.ZE_ENV_DESCRIPTION || 'Fallback Description';
+  const title = process.env.ZE_ENV_TITLE || 'Fallback Title';
+  const description = process.env.ZE_ENV_DESCRIPTION || 'Fallback Description';
 
   return (
     <div>

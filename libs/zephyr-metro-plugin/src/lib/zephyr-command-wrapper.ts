a8757bcae0c09741cae104b60abd392c053dc5b2
theGrep01 <denis.belotrserkovets@gmail.com>
2025-05-23 15:10:43 +0300

    feat: logs

--- libs/zephyr-metro-plugin/src/lib/zephyr-command-wrapper.ts
+++ libs/zephyr-metro-plugin/src/lib/zephyr-command-wrapper.ts
@@ -1,20 +1,36 @@
-import { ZephyrEngine, ze_log } from 'zephyr-agent';
+import { Platform, ZephyrEngine, ze_log } from 'zephyr-agent';
+import { extractFederatedDependencyPairs } from 'zephyr-xpack-internal';
 
 export interface ZephyrCommandWrapperConfig {
-  platform: string;
+  platform: Platform;
   mode: string;
   context: string;
 }
 
-export async function zephyrCommandWrapper(config: ZephyrCommandWrapperConfig) {  
-    console.log('Zephyr command wrapper');
+export async function zephyrCommandWrapper(config: ZephyrCommandWrapperConfig) {
+  console.log('Zephyr command wrapper');
 
-    const zephyr_engine = await ZephyrEngine.create({
-      builder: 'metro',
-      context: config.context,
-    });
-    ze_log('Configuring with Zephyr... \n config: ', config);
+  const zephyr_engine = await ZephyrEngine.create({
+    builder: 'metro',
+    context: config.context,
+  });
+  ze_log('Configuring with Zephyr... \n config: ', config);
 
-    return;
-};
+  zephyr_engine.env.target = config.platform;
 
+  const dependency_pairs = extractFederatedDependencyPairs(config);
+
+  ze_log(
+    'Resolving and building towards target by zephyr_engine.env.target: ',
+    zephyr_engine.env.target
+  );
+
+  const resolved_dependency_pairs =
+    await zephyr_engine.resolve_remote_dependencies(dependency_pairs);
+
+  ze_log('dependency resolution completed successfully...or at least trying to...');
+
+  ze_log('Application uid created...');
+
+  return;
+}

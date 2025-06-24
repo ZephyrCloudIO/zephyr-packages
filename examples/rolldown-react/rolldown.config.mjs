09b4a4a2c291355624dc906ebacb8da6aec249af
zmzlois <lois.z@comcord.vision>
2025-06-24 16:26:38 -0700

    chore: clean up rolldown

--- examples/rolldown-react/rolldown.config.mjs
+++ examples/rolldown-react/rolldown.config.mjs
@@ -2,8 +2,26 @@ import { defineConfig } from 'rolldown';
 import { withZephyr } from 'zephyr-rolldown-plugin';
 
 export default defineConfig({
-  input: {
-    main: 'src/main.tsx',
-  },
-  plugins: [withZephyr()],
+  input: 'src/main.tsx',
+  plugins: [
+    {
+      name: 'emit-html',
+      generateBundle() {
+        const html = `
+          <html>
+            <body>
+              <div id="root"></div>
+              <script type="module" src="./main.js"></script>
+            </body>
+          </html>
+        `;
+        this.emitFile({
+          type: 'asset',
+          fileName: 'index.html',
+          source: html,
+        });
+      },
+    },
+    withZephyr(),
+  ],
 });

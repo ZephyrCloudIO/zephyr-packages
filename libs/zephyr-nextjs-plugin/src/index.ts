cd3fe3e31c29c053edebc827d4d996010f8b6a93
Néstor <nestor@nstlopez.com>
2025-07-02 07:46:44 -0700

    feat: remove nextjs plugin

--- libs/zephyr-nextjs-plugin/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { withZephyr } from './nextjs-plugin/with-zephyr';
-
-// hacks
-export { onDeploymentDone } from 'zephyr-xpack-internal';
-export { resolveIndexHtml } from 'zephyr-agent';
--- libs/zephyr-nextjs-plugin/src/types/missing-webpack-types.ts
+++
@@ -1,4 +0,0 @@
-import type { Compiler } from 'webpack';
-import type { XPackConfiguration } from 'zephyr-xpack-internal';
-
-export type WebpackConfiguration = XPackConfiguration<Compiler>;

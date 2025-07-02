0cd9f696426a56779e25e6f1d22b738d8402ecf4
Néstor <nestor@nstlopez.com>
2025-07-02 09:04:10 -0700

    chore: more cleanu

--- examples/nextjs-15/next.config.js
+++ examples/nextjs-15/next.config.js
@@ -1,19 +1,3 @@
-// const { withZephyr } = require('zephyr-nextjs-plugin');
-
-// /** @type {import('next').Nextonfig} */
-// const nextConfig = {
-//   webpack: (config, context) => {
-//     // console.log('Webpack configuration:', context);
-//     return withZephyr()(config, context);
-//   },
-
-// };
-
-// module.exports = function (args, arg1, ...rest) {
-// console.log('Arguments received:', args, arg1, rest);
-
-//   return nextConfig
-// };
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {

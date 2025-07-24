5542ef6b12aa6c2617266010ff3873abd7ed8b24
nitink23 <50839741+nitink23@users.noreply.github.com>
2025-07-24 09:48:01 -0700

    feat: removed all docker files + API endpoint appraoch

--- libs/zephyr-agent/src/lib/telemetry/index.ts
+++ libs/zephyr-agent/src/lib/telemetry/index.ts
@@ -2,31 +2,51 @@
 // Best practices: explicit async init, idempotency, auto-instrumentation, resource attributes, shutdown, config flexibility, metrics-ready
 
 import { trace, type Tracer } from '@opentelemetry/api';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+//import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { OTLPTraceExporter as OTLPTraceExporterGrpc } from '@opentelemetry/exporter-trace-otlp-grpc';
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import type {
-  SpanProcessor} from '@opentelemetry/sdk-trace-base';
-import {
-  ConsoleSpanExporter,
-  SimpleSpanProcessor
-} from '@opentelemetry/sdk-trace-base';
+import type { SpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.join(__dirname, '../../../.env') });
 
 let sdk: NodeSDK | undefined;
 let initialized = false;
-const OTLP_ENDPOINT = 'http://localhost:4318';
+const OTLP_ENDPOINT = process.env['OTEL_EXPORTER_OTLP_ENDPOINT'];
+const OTEL_EXPORTER_OTLP_HEADERS = process.env['OTEL_EXPORTER_OTLP_HEADERS'];
 
+// this is to parse the headers from the environment variable
+function parseHeaders(headers: string): Record<string, string> {
+  return headers.split(',').reduce(
+    (acc, header) => {
+      const [key, value] = header.split('=');
+      acc[key] = value;
+      return acc;
+    },
+    {} as Record<string, string>
+  );
+}
 export async function initTelemetry(): Promise<void> {
   if (initialized) return;
 
-  const otlpExporter = new OTLPTraceExporter({ url: OTLP_ENDPOINT });
+  // starting the otlp exporter this is ONLY for the traces
+  const otlpExporter = new OTLPTraceExporterGrpc({
+    url: `${OTLP_ENDPOINT}/v1/traces`,
+    headers: parseHeaders(OTEL_EXPORTER_OTLP_HEADERS || 'Not found environemnt headers'),
+  });
   const consoleExporter = new ConsoleSpanExporter();
+
+  // TODO: Add metrics exporter
+
+  // TODO: Add logging exporter
 
   sdk = new NodeSDK({
     spanProcessors: [
       new SimpleSpanProcessor(otlpExporter),
-      new SimpleSpanProcessor(consoleExporter),
+      new SimpleSpanProcessor(consoleExporter), // TODO: Remove this once we have a proper logging system
     ] as SpanProcessor[],
-    // No auto-instrumentations
+    // No auto-instrumentations might add later if we need it
   });
 
   await sdk.start();
@@ -36,3 +56,6 @@ export async function initTelemetry(): Promise<void> {
 export function getTracer(name: string): Tracer {
   return trace.getTracer(name);
 }
+
+// TODO: Add metrics
+// TODO: Add logging

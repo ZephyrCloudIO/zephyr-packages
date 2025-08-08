76e2fe0a5ca40026c5cf545556242677aed10d25
Valerii Tropin <tropin.valeriy@gmail.com>
2025-08-08 16:03:12 +0300

    feat: aws integration

--- libs/zephyr-agent/src/lib/deployment/aws-upload.strategy.ts
+++ libs/zephyr-agent/src/lib/deployment/aws-upload.strategy.ts
@@ -1,14 +1,19 @@
+import { ze_log } from 'zephyr-agent';
+
 import type { UploadOptions, ZephyrEngine } from '../../zephyr-engine';
-import { commonUploadStrategy } from './common-upload.strategy';
 import { ZeErrors, ZephyrError } from '../errors';
+import { getApplicationConfiguration } from '../edge-requests/get-application-configuration';
+import { makeRequest } from '../http/http-request';
+import { zeUploadSnapshot } from '../edge-actions';
+import { uploadAssets, uploadBuildStatsAndEnableEnvs } from './upload-base';
 
 const EDGE_LAMBDA_MAX_BODY_SIZE = 788403; // floor(1024 * 1024 / 1.33), where 1.33 is size overhead of base64 encoding
 
 export async function awsUploadStrategy(
   zephyr_engine: ZephyrEngine,
-  uploadOptions: UploadOptions,
+  { snapshot, getDashData, assets: { assetsMap, missingAssets } }: UploadOptions,
 ): Promise<string> {
-  const snapshotSize = Buffer.byteLength(JSON.stringify(uploadOptions.snapshot), 'utf8');
+  const snapshotSize = Buffer.byteLength(JSON.stringify(snapshot), 'utf8');
   if (snapshotSize > EDGE_LAMBDA_MAX_BODY_SIZE) {
     const opts = {
       entity_name: 'snapshot',
@@ -18,7 +23,7 @@ export async function awsUploadStrategy(
     throw new ZephyrError(ZeErrors.ERR_MAX_PAYLOAD_SIZE_EXCEEDED, opts);
   }
 
-  for (const missingAsset of uploadOptions.assets.missingAssets) {
+  for (const missingAsset of missingAssets) {
     if (missingAsset.size > EDGE_LAMBDA_MAX_BODY_SIZE) {
       const opts = {
         entity_name: missingAsset.path,
@@ -29,5 +34,42 @@ export async function awsUploadStrategy(
     }
   }
 
-  return commonUploadStrategy(zephyr_engine, uploadOptions);
+  await createBucket(zephyr_engine.application_uid);
+  await uploadAssets(zephyr_engine, { assetsMap, missingAssets });
+  const versionUrl = await zeUploadSnapshot(zephyr_engine, { snapshot });
+
+  // Waits for the reply to check upload problems, but the reply is a simply
+  // 200 OK sent before any processing
+  await uploadBuildStatsAndEnableEnvs(zephyr_engine, { getDashData, versionUrl });
+
+  return versionUrl;
+}
+
+export async function createBucket(application_uid: string): Promise<void> {
+  const { EDGE_URL, jwt } = await getApplicationConfiguration({
+    application_uid,
+  });
+
+  // const body = '';
+
+  const options: RequestInit = {
+    method: 'POST',
+    headers: {
+      can_write_jwt: jwt,
+    },
+  };
+
+  const url = new URL('/upload', EDGE_URL);
+  url.searchParams.append('type', 'bucket');
+
+  const [ok, cause] = await makeRequest(url, options);
+
+  if (!ok) {
+    throw new ZephyrError(ZeErrors.ERR_FAILED_UPLOAD, {
+      type: 'bucket',
+      cause,
+    });
+  }
+
+  ze_log.snapshot('Done: bucket initialized...');
 }

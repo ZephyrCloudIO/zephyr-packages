bfb16639fefd9c3af061862a7cdde5dc0e1dc266
Donny/강동윤 <kdy1997.dev@gmail.com>
2025-11-11 13:31:48 +0900

    chore: Configure content patching

--- libs/zephyr-agent/src/lib/transformers/ze-build-assets-map.ts
+++ libs/zephyr-agent/src/lib/transformers/ze-build-assets-map.ts
@@ -1,5 +1,6 @@
 import type { ZeBuildAssetsMap } from 'zephyr-edge-contract';
 
+import { transformSync } from '@swc/core';
 import { ze_log } from '../logging';
 import { zeBuildAssets } from './ze-build-assets';
 
@@ -20,11 +21,38 @@ export function buildAssetsMapMock<T>(
 ) {
   return Object.keys(assets).reduce((memo, filepath) => {
     const asset = assets[filepath];
-    const buffer = extractBuffer(asset);
+    let buffer = extractBuffer(asset);
 
     if (!buffer && buffer !== '') {
       ze_log.upload(`unknown asset type: ${getAssetType(asset)}`);
       return memo;
+    }
+
+    if (filepath.endsWith('.js')) {
+      const content = buffer.toString();
+      console.log('original content: ', content);
+      const compiled = transformSync(content, {
+        jsc: {
+          target: 'esnext',
+          transform: {
+            optimizer: {
+              globals: {
+                envs: {
+                  ZE_PUBLIC_LOG: '0',
+                  ZE_PUBLIC_COND: '0',
+                },
+              },
+            },
+          },
+          minify: {
+            // compress: true,
+            // mangle: true,
+          },
+        },
+        // minify: true,
+      });
+      console.log('compiled content: ', compiled.code);
+      buffer = compiled.code;
     }
 
     const assetMap = zeBuildAssets({ filepath, content: buffer });

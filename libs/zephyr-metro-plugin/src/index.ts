681c6e69357df27d88def7a902d5483c1fc4ba9d
zhongwuzw <zhongwuzw@qq.com>
2026-03-26 23:31:30 +0800

    chore(zephyr-metro-plugin): remove zephyrCommandWrapper export

--- libs/zephyr-metro-plugin/src/index.ts
+++ libs/zephyr-metro-plugin/src/index.ts
@@ -8,8 +8,5 @@ export {
   type ZephyrModuleFederationConfig
 } from './lib/with-zephyr';
 
-// Command wrapper for react-native.config.js integration
-export { zephyrCommandWrapper } from './lib/zephyr-metro-command-wrapper';
-
 // Transformer (usually not imported directly but referenced by path)
 export { transform as zephyrTransformer } from './lib/zephyr-transformer';
